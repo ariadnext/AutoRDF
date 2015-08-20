e7e0509aa3d0fa1177367d3e28196fe1a0148ba8
Fabien Chevalier <fabien.chevalier@ariadnext.com>
2015-08-20 15:36:30 +0000

    Support for more Data Types

--- include/autordf/cvt/Cvt.h
+++ include/autordf/cvt/Cvt.h
@@ -18,11 +18,35 @@ namespace cvt {
     X(xsd_float) \
     X(xsd_double) \
     X(xsd_dateTime) \
-    X(xsd_int)
+    X(xsd_integer) \
+    X(xsd_dateTimeStamp) \
+    X(xsd_nonNegativeInteger) \
+    X(xsd_positiveInteger) \
+    X(xsd_nonPositiveInteger) \
+    X(xsd_negativeInteger) \
+    X(xsd_long) \
+    X(xsd_unsignedLong) \
+    X(xsd_int) \
+    X(xsd_unsignedInt) \
+    X(xsd_short) \
+    X(xsd_unsignedShort) \
+    X(xsd_byte) \
+    X(xsd_unsignedByte)
+
+// Integer types are a pain. Mapping from/to C++ <--> XML Schema is done using info from
+// - http://en.cppreference.com/w/cpp/language/types
+// and
+// - http://www.w3.org/TR/xmlschema-2
+//
+// 1 - We make sure that data types association between both world are at least big enough to be sure that
+// any data read out there will fit in the C++ type, except for xsd:decimal and xsd:*integer, where we merely rely on an approximation
+// We approximate xsd:decimal with double and xsd:*integer with long long.
+// 2 - However it is responsibility of the user to make sure
+// that the data he puts in his C++ variables will fit in the corresponding XSD types
 
 enum class RdfTypeEnum {
 #define X(a) a,
-    CVT_TYPES_DEF(X)
+   CVT_TYPES_DEF(X)
 #undef X
 };
 
@@ -63,13 +87,30 @@ inline std::string getLeft(const std::string& rawValue) {
     }
 }
 
-template<typename CppType, RdfTypeEnum rdfType> class toCpp {
-public:
-    static CppType val(const std::string& rawValue) {
-        return  boost::lexical_cast<CppType>(trim(getLeft(rawValue)));
-    }
+template<typename CppType, RdfTypeEnum rdfType> class toCpp;
+
+template<typename CppType> CppType toCppGeneric(const std::string& rawValue) {
+    return boost::lexical_cast<CppType>(trim(getLeft(rawValue)));
+}
+
+inline boost::posix_time::ptime toCppDateTime(const std::string& rawValue) {
+    std::istringstream ss(trim(getLeft(rawValue)));
+    ss.exceptions(std::ios_base::failbit);
+    boost::posix_time::time_input_facet* input_facet = new boost::posix_time::time_input_facet;
+    input_facet->format("%Y-%m-%dT%H:%M:%S%F%Q");
+    ss.imbue(std::locale(std::locale::classic(), input_facet));
+    boost::local_time::local_date_time result(boost::date_time::not_a_date_time);
+    ss >> result;
+    return result.utc_time();
 };
 
+#define GENERIC_TOCPP(cppType, rdfType) \
+template<> class toCpp<cppType, RdfTypeEnum::rdfType> { \
+public: \
+    static cppType val(const std::string& rawValue) { return toCppGeneric<cppType>(rawValue); } \
+}; \
+
+// xs:string
 template<RdfTypeEnum rdfType> class toCpp<std::string, rdfType> {
 public:
     static std::string val(const std::string& rawValue) {
@@ -77,6 +118,19 @@ public:
     }
 };
 
+// xsd:integer
+GENERIC_TOCPP(long long int, xsd_integer)
+
+// xsd:decimal
+GENERIC_TOCPP(double, xsd_decimal)
+
+// xsd:float
+GENERIC_TOCPP(float, xsd_float)
+
+// xsd:double
+GENERIC_TOCPP(double, xsd_double)
+
+// xsd_boolean
 template<typename CppType> class toCpp<CppType, RdfTypeEnum::xsd_boolean> {
 public:
     static CppType val(const std::string& rawValue) {
@@ -91,43 +145,181 @@ public:
     }
 };
 
+// xsd:dateTime
 template<> class toCpp<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTime> {
 public:
     static boost::posix_time::ptime val(const std::string& rawValue) {
-        std::stringstream ss(trim(getLeft(rawValue)));
-        ss.exceptions(std::ios_base::failbit);
-        boost::local_time::local_time_input_facet* input_facet = new boost::local_time::local_time_input_facet;
-        input_facet->set_iso_extended_format();
-        ss.imbue(std::locale(std::locale::classic(), input_facet));
-        boost::local_time::local_date_time result(boost::date_time::not_a_date_time);
-        ss >> result;
-        return result.utc_time();
+        return toCppDateTime(rawValue);
     }
 };
 
-template<typename CppType, RdfTypeEnum rdfType> class toRdf {
+// xsd:dateTimeStamp
+template<> class toCpp<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTimeStamp> {
+public:
+    static boost::posix_time::ptime val(const std::string& rawValue) {
+        return toCppDateTime(rawValue);
+    }
+};
+
+// xsd:nonNegativeInteger
+GENERIC_TOCPP(long long unsigned int, xsd_nonNegativeInteger)
+
+// xsd:positiveInteger
+GENERIC_TOCPP(long long unsigned int, xsd_positiveInteger)
+
+// xsd:nonPositiveInteger
+GENERIC_TOCPP(long long int, xsd_nonPositiveInteger)
+
+// xsd:negativeInteger
+GENERIC_TOCPP(long long int, xsd_negativeInteger)
+
+// xsd:long
+GENERIC_TOCPP(long long int, xsd_long)
+
+// xsd:unsignedLong
+GENERIC_TOCPP(long long unsigned int, xsd_unsignedLong)
+
+// xsd:int
+GENERIC_TOCPP(long int, xsd_int)
+
+// xsd:unsignedInt
+GENERIC_TOCPP(long unsigned int, xsd_unsignedInt)
+
+// xsd:short
+GENERIC_TOCPP(short, xsd_short)
+
+// xsd:unsignedShort
+GENERIC_TOCPP(unsigned short, xsd_unsignedShort)
+
+// xsd:byte
+template<> class toCpp<char, RdfTypeEnum::xsd_byte> {
+public:
+    static char val(const std::string& rawValue) {
+        return boost::lexical_cast<short>(trim(getLeft(rawValue)));
+    }
+};
+
+// xsd:unsignedByte
+template<> class toCpp<unsigned char, RdfTypeEnum::xsd_unsignedByte> {
+public:
+    static unsigned char val(const std::string& rawValue) {
+        return boost::lexical_cast<unsigned short>(trim(getLeft(rawValue)));
+    }
+};
+
+template<typename CppType, RdfTypeEnum rdfType> class toRdf;
+
+template<RdfTypeEnum rdfType, typename CppType> std::string toRdfGeneric(const CppType& cppValue) {
+    std::ostringstream ss;
+    ss << "\"" << cppValue << "\"^^" << rdfTypeEnumString(rdfType);
+    return ss.str();
+};
+
+#define GENERIC_TORDF(cppType, rdfType) \
+template<> class toRdf<cppType, RdfTypeEnum::rdfType> { \
+public: \
+    static std::string val(const cppType& cppValue) { return toRdfGeneric<RdfTypeEnum::rdfType>(cppValue); } \
+};
+
+// xsd:string
+GENERIC_TORDF(std::string, xsd_string)
+
+// xsd:integer
+GENERIC_TORDF(long long int, xsd_integer)
+
+// xsd:decimal
+GENERIC_TORDF(double, xsd_decimal)
+
+// xsd:float
+GENERIC_TORDF(float, xsd_float)
+
+// xsd:double
+GENERIC_TORDF(double, xsd_double)
+
+// xsd:boolean
+template<typename CppType> class toRdf<CppType, RdfTypeEnum::xsd_boolean>  {
 public:
     static std::string val(const CppType& cppValue) {
+        return std::string("\"") + (cppValue ? "true" : "false") + "\"^^" + rdfTypeEnumString(RdfTypeEnum::xsd_boolean);
+    }
+};
+
+inline std::string toRdfDateTime(const boost::posix_time::ptime& time, RdfTypeEnum type) {
+    std::ostringstream ss;
+    ss.exceptions(std::ios_base::failbit);
+    boost::posix_time::time_facet* facet = new boost::posix_time::time_facet;
+    // ISO Extended with Z as forced timezone
+    facet->format("%Y-%m-%dT%H:%M:%S%FZ");
+    ss.imbue(std::locale(std::locale::classic(), facet));
+    ss << "\"" << time << "\"^^" << rdfTypeEnumString(type);
+    return ss.str();
+}
+
+// xsd:dateTime
+template<> class toRdf<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTime> {
+public:
+    static std::string val(const boost::posix_time::ptime& time) {
+        return toRdfDateTime(time, RdfTypeEnum::xsd_dateTime);
+    }
+};
+
+// xsd:dateTimeStamp
+template<> class toRdf<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTimeStamp> {
+public:
+    static std::string val(const boost::posix_time::ptime& time) {
+        return toRdfDateTime(time, RdfTypeEnum::xsd_dateTimeStamp);
+    }
+};
+
+// xsd:nonNegativeInteger
+GENERIC_TORDF(long long unsigned int, xsd_nonNegativeInteger)
+
+// xsd:positiveInteger
+GENERIC_TORDF(long long unsigned int, xsd_positiveInteger)
+
+// xsd:nonPositiveInteger
+GENERIC_TORDF(long long int, xsd_nonPositiveInteger)
+
+// xsd:negativeInteger
+GENERIC_TORDF(long long int, xsd_negativeInteger)
+
+// xsd:long
+GENERIC_TORDF(long long int, xsd_long)
+
+// xsd:unsignedLong
+GENERIC_TORDF(long long unsigned int, xsd_unsignedLong)
+
+// xsd:int
+GENERIC_TORDF(long int, xsd_int)
+
+// xsd:unsignedInt
+GENERIC_TORDF(long unsigned int, xsd_unsignedInt)
+
+// xsd:short
+GENERIC_TORDF(short, xsd_short)
+
+// xsd:unsignedShort
+GENERIC_TORDF(unsigned short, xsd_unsignedShort)
+
+// xsd:byte
+template<> class toRdf<char, RdfTypeEnum::xsd_byte>  {
+public:
+    static std::string val(char cppValue) {
         std::ostringstream ss;
-        ss << "\"" << cppValue << "\"^^" << rdfTypeEnumString(rdfType);
+        ss << "\"" << static_cast<short>(cppValue) << "\"^^" << rdfTypeEnumString(RdfTypeEnum::xsd_byte);
         return ss.str();
     }
 };
 
-template<typename CppType> class toRdf<CppType, RdfTypeEnum::xsd_boolean>  {
+// xsd:unsignedByte
+template<> class toRdf<unsigned char, RdfTypeEnum::xsd_unsignedByte>  {
 public:
-    static std::string val(const CppType& cppValue) {
-        return  std::string("\"") + (cppValue ? "true" : "false") + "\"^^" + rdfTypeEnumString(RdfTypeEnum::xsd_boolean);
+    static std::string val(unsigned char cppValue) {
+        std::ostringstream ss;
+        ss << "\"" << static_cast<unsigned short>(cppValue) << "\"^^" << rdfTypeEnumString(RdfTypeEnum::xsd_unsignedByte);
+        return ss.str();
     }
 };
-
-template<> class toRdf<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTime> {
-public:
-    static std::string val(const boost::posix_time::ptime& time) {
-        return  std::string("\"") + boost::posix_time::to_iso_extended_string(time) + "Z\"^^" + rdfTypeEnumString(RdfTypeEnum::xsd_dateTime);
-    }
-};
-
 }
 }
 #endif //AUTORDF_CVT_CVT_H
--- src/autordf/unittests/04_cvtUnitTests.cpp
+++ src/autordf/unittests/04_cvtUnitTests.cpp
@@ -8,37 +8,61 @@ using namespace autordf::cvt;
 
 TEST(_04_cvt, xsd_to_cpp) {
     ASSERT_EQ("date of birth", (toCpp<std::string, RdfTypeEnum::xsd_string>::val("\"date of birth\"^^xsd:string")));
-    ASSERT_EQ(210, (toCpp<int, RdfTypeEnum::xsd_int>::val("\"210\"^^xsd:int")));
+    ASSERT_EQ(-210, (toCpp<long long int, RdfTypeEnum::xsd_integer>::val("\"-210\"^^xsd:integer")));
+    ASSERT_EQ(210, (toCpp<long long unsigned int, RdfTypeEnum::xsd_nonNegativeInteger>::val("\"210\"^^xsd:nonNegativeInteger")));
+    ASSERT_EQ(210, (toCpp<long long unsigned int, RdfTypeEnum::xsd_positiveInteger>::val("\"210\"^^xsd:positiveInteger")));
+    ASSERT_EQ(-210, (toCpp<long long int, RdfTypeEnum::xsd_negativeInteger>::val("\"-210\"^^xsd:negativeInteger")));
+    ASSERT_EQ(-210, (toCpp<long long int, RdfTypeEnum::xsd_nonPositiveInteger>::val("\"-210\"^^xsd:nonPositiveInteger")));
+    ASSERT_EQ(210, (toCpp<long long unsigned int, RdfTypeEnum::xsd_unsignedLong>::val("\"210\"^^xsd:unsignedLong")));
+    ASSERT_EQ(-210, (toCpp<long long int, RdfTypeEnum::xsd_long>::val("\"-210\"^^xsd:long")));
+    ASSERT_EQ(210, (toCpp<long unsigned int, RdfTypeEnum::xsd_unsignedInt>::val("\"210\"^^xsd:unsignedInt")));
+    ASSERT_EQ(-210, (toCpp<long int, RdfTypeEnum::xsd_int>::val("\"-210\"^^xsd:int")));
+    ASSERT_EQ(210, (toCpp<unsigned short, RdfTypeEnum::xsd_unsignedShort>::val("\"210\"^^xsd:unsignedShort")));
+    ASSERT_EQ(-210, (toCpp<short, RdfTypeEnum::xsd_short>::val("\"-210\"^^xsd:short")));
+    ASSERT_EQ(127, (toCpp<unsigned char, RdfTypeEnum::xsd_unsignedByte>::val("\"127\"^^xsd:unsignedByte")));
+    ASSERT_EQ(-127, (toCpp<char, RdfTypeEnum::xsd_byte>::val("\"-127\"^^xsd:byte")));
     ASSERT_EQ(true, (toCpp<bool, RdfTypeEnum::xsd_boolean>::val("\"true\"^^xsd:boolean")));
     ASSERT_EQ(true, (toCpp<bool, RdfTypeEnum::xsd_boolean>::val("\"1\"^^xsd:boolean")));
     ASSERT_EQ(false, (toCpp<bool, RdfTypeEnum::xsd_boolean>::val("\"0\"^^xsd:boolean")));
-    ASSERT_EQ(0.1, (toCpp<double, RdfTypeEnum::xsd_decimal>::val("\"0.1\"^^xsd:decimal")));
+    ASSERT_EQ(0.5, (toCpp<double, RdfTypeEnum::xsd_decimal>::val("\"0.5\"^^xsd:decimal")));
     ASSERT_EQ(0.5, (toCpp<float, RdfTypeEnum::xsd_float>::val("\"0.5\"^^xsd:float")));
     ASSERT_EQ(0.5, (toCpp<double, RdfTypeEnum::xsd_double>::val("\"0.5\"^^xsd:double")));
-    ASSERT_EQ("1977-Apr-22 06:00:00", boost::posix_time::to_simple_string(toCpp<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTime>::val("\"1977-04-22T06:00:00Z\"^^xsd:dateTime")));
+    ASSERT_EQ("1977-Apr-22 06:00:00", boost::posix_time::to_simple_string(toCpp<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTime>::val("\"1977-04-22T06:00:00\"^^xsd:dateTime")));
+    ASSERT_EQ("1977-Apr-22 06:00:00", boost::posix_time::to_simple_string(toCpp<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTimeStamp>::val("\"1977-04-22T06:00:00Z\"^^xsd:dateTimeStamp")));
 
     //Checking trimming
     ASSERT_EQ("date of birth  ", (toCpp<std::string, RdfTypeEnum::xsd_string>::val("\"date of birth  \"^^xsd:string")));
     ASSERT_EQ(true, (toCpp<bool, RdfTypeEnum::xsd_boolean>::val("\"1 \"^^xsd:boolean")));
     ASSERT_EQ(false, (toCpp<bool, RdfTypeEnum::xsd_boolean>::val("\" 0\"^^xsd:boolean")));
-    ASSERT_EQ(210, (toCpp<int, RdfTypeEnum::xsd_int>::val("\"\t210\n\"^^xsd:int")));
+    ASSERT_EQ(210, (toCpp<long long, RdfTypeEnum::xsd_integer>::val("\"\t210\n\"^^xsd:integer")));
 }
 
 TEST(_04_cvt, cpp_to_xsd) {
     ASSERT_EQ("\"date of birth\"^^xsd:string", (toRdf<std::string, RdfTypeEnum::xsd_string>::val("date of birth")));
-    ASSERT_EQ("\"210\"^^xsd:int", (toRdf<int, RdfTypeEnum::xsd_int>::val(210)));
+    ASSERT_EQ("\"-210\"^^xsd:integer", (toRdf<long long int, RdfTypeEnum::xsd_integer>::val(-210)));
+    ASSERT_EQ("\"210\"^^xsd:nonNegativeInteger", (toRdf<long long unsigned int, RdfTypeEnum::xsd_nonNegativeInteger>::val(210)));
+    ASSERT_EQ("\"210\"^^xsd:positiveInteger", (toRdf<long long unsigned int, RdfTypeEnum::xsd_positiveInteger>::val(210)));
+    ASSERT_EQ("\"-210\"^^xsd:negativeInteger", (toRdf<long long int, RdfTypeEnum::xsd_negativeInteger>::val(-210)));
+    ASSERT_EQ("\"-210\"^^xsd:nonPositiveInteger", (toRdf<long long int, RdfTypeEnum::xsd_nonPositiveInteger>::val(-210)));
+    ASSERT_EQ("\"210\"^^xsd:unsignedLong", (toRdf<long long unsigned int, RdfTypeEnum::xsd_unsignedLong>::val(210)));
+    ASSERT_EQ("\"-210\"^^xsd:long", (toRdf<long long int, RdfTypeEnum::xsd_long>::val(-210)));
+    ASSERT_EQ("\"210\"^^xsd:unsignedInt", (toRdf<long unsigned int, RdfTypeEnum::xsd_unsignedInt>::val(210)));
+    ASSERT_EQ("\"-210\"^^xsd:int", (toRdf<long int, RdfTypeEnum::xsd_int>::val(-210)));
+    ASSERT_EQ("\"210\"^^xsd:unsignedShort", (toRdf<unsigned short, RdfTypeEnum::xsd_unsignedShort>::val(210)));
+    ASSERT_EQ("\"-210\"^^xsd:short", (toRdf<short, RdfTypeEnum::xsd_short>::val(-210)));
+    ASSERT_EQ("\"127\"^^xsd:unsignedByte", (toRdf<unsigned char, RdfTypeEnum::xsd_unsignedByte>::val(127)));
+    ASSERT_EQ("\"-127\"^^xsd:byte", (toRdf<char, RdfTypeEnum::xsd_byte>::val(-127)));
     ASSERT_EQ("\"true\"^^xsd:boolean", (toRdf<bool, RdfTypeEnum::xsd_boolean>::val(true)));
     // That works too
     ASSERT_EQ("\"true\"^^xsd:boolean", (toRdf<int, RdfTypeEnum::xsd_boolean>::val(5)));
     // That works too
     ASSERT_EQ("\"false\"^^xsd:boolean", (toRdf<float, RdfTypeEnum::xsd_boolean>::val(0.0)));
 
-    ASSERT_EQ("\"0.1\"^^xsd:decimal", (toRdf<float, RdfTypeEnum::xsd_decimal>::val(0.1)));
-    ASSERT_EQ("\"0.1\"^^xsd:decimal", (toRdf<double, RdfTypeEnum::xsd_decimal>::val(0.1)));
-    ASSERT_EQ("\"0.1\"^^xsd:decimal", (toRdf<long double, RdfTypeEnum::xsd_decimal>::val(0.1)));
+    ASSERT_EQ("\"0.5\"^^xsd:decimal", (toRdf<double, RdfTypeEnum::xsd_decimal>::val(0.5)));
     ASSERT_EQ("\"0.5\"^^xsd:float", (toRdf<float, RdfTypeEnum::xsd_float>::val(0.5)));
     ASSERT_EQ("\"0.5\"^^xsd:double", (toRdf<double, RdfTypeEnum::xsd_double>::val(0.5)));
     ASSERT_EQ("\"2002-01-20T23:59:59.001000Z\"^^xsd:dateTime", (toRdf<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTime>::val(boost::posix_time::time_from_string("2002-01-20 23:59:59.001"))));
+    ASSERT_EQ("\"2002-01-20T23:59:59.001000Z\"^^xsd:dateTimeStamp", (toRdf<boost::posix_time::ptime, RdfTypeEnum::xsd_dateTimeStamp>::val(boost::posix_time::time_from_string("2002-01-20 23:59:59.001"))));
 }
 
 // This is cuurently noot supported by boost
