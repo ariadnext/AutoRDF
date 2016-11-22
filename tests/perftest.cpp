7643bbab55c4ac0488525c39b6b878fda15b4522
Fabien Chevalier <fabien.chevalier@ariadnext.com>
2016-11-22 14:41:12 +0000

    Add performance test

--- tests/perftest.cpp
+++ tests/perftest.cpp
@@ -1,9 +1,35 @@
-#include <gtest/gtest.h>
-
 #include <locale>
 
-int main(int argc, char **argv) {
-  ::setlocale(LC_CTYPE, "");
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+#include <autordf/Factory.h>
+#include <autordf/Storage.h>
+#include <autordf/Object.h>
+
+using namespace autordf;
+
+int main(int, char **) {
+    Factory f(std::make_shared<Storage>("hashes", "db1",
+            "hash-type='bdb'"));
+    Object::setFactory(&f);
+
+    static unsigned int OBJECTS_COUNT = 3000;
+
+    for ( unsigned int i = 0; i < OBJECTS_COUNT; i++) {
+        std::stringstream ss;
+        ss << "http://myobject" << i;
+        Object o(ss.str());
+        for ( unsigned int j = 0; j < 10; ++j ) {
+            std::stringstream p;
+            p << "http://property" << j;
+            std::stringstream v;
+            v << "http://value" << j;
+            o.setPropertyValue(p.str(), v.str());
+        }
+    }
+
+    for ( unsigned int i = 0; i < OBJECTS_COUNT; i++) {
+        std::stringstream ss;
+        ss << "http://myobject" << i;
+        Object o(ss.str());
+        o.remove();
+    }
 }
