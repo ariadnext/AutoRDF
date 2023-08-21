a127a6c1ff0abb755cf99f93f1d4a55ab56f473e
Pierre MONTEMBAULT <pierre.montembault@ariadnext.com>
2023-08-21 09:01:07 +0000

    [VARDA-1109] Refactor validator

--- src/autordf/ontology/unittests/01_ValidatorUnitTests.cpp
+++ src/autordf/ontology/unittests/01_ValidatorUnitTests.cpp
@@ -19,21 +19,19 @@ using namespace ontology;
 class ValidatorTest : public ::testing::Test {
 public:
     autordf::Factory factory;
-    std::shared_ptr<Validator> validator;
     std::shared_ptr<Ontology> ontology;
 
-    void SetUp() {
+    void SetUp() override{
         Object::setFactory(&factory);
         factory.loadFromFile(boost::filesystem::path(__FILE__).parent_path().string() + "/geometry-ontology.ttl");
         factory.loadFromFile(boost::filesystem::path(__FILE__).parent_path().string() + "/geometry.ttl");
         factory.loadFromFile(boost::filesystem::path(__FILE__).parent_path().string() + "/schema.ttl");
         ontology = std::make_shared<Ontology>(&factory);
-        validator = std::make_shared<Validator>(ontology);
     }
 
-    static void dumpErrors(const std::shared_ptr<std::vector<Validator::Error>>& errors) {
+    static void dumpErrors(const validation::Error::Vec & errors) {
         return; // comment this return to dump errors (usefull when modifying these tests)
-        for (auto const& error : *errors) {
+        for (auto const& error : errors) {
             std::cout << error.fullMessage() << std::endl;
         }
     }
@@ -41,138 +39,138 @@ public:
 
 TEST_F(ValidatorTest, MissingPropertyExactly1) {
     const Object obj("http://example.org/geometry#pointWithoutY");
-    const auto errors = validator->validateObject(obj);
+    const auto errors = validation::validateObject(*ontology, obj);
 
-    ASSERT_EQ(1, errors->size());
-    EXPECT_EQ("'pointWithoutY' property 'y' has 0 distinct values. Minimum allowed is 1", errors->at(0).fullMessage());
-    EXPECT_EQ("http://example.org/geometry#y", std::string{errors->at(0).property.iri()});
-    EXPECT_EQ(Validator::Error::NOTENOUHVALUES, errors->at(0).type);
+    ASSERT_EQ(1, errors.size());
+    EXPECT_EQ("'pointWithoutY' property 'y' has 0 distinct values. Minimum allowed is 1", errors.at(0).fullMessage());
+    EXPECT_EQ("http://example.org/geometry#y", std::string{errors.at(0).property.iri()});
+    EXPECT_EQ(validation::Error::NOTENOUHVALUES, errors.at(0).type);
 }
 
 
 TEST_F(ValidatorTest, ObjectValidator) {
     const Object obj("http://example.org/geometry#myShape");
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj);
+    const auto errors = validation::validateObject(*ontology, obj);
     dumpErrors(errors);
 
-    ASSERT_EQ(3, errors->size());
+    ASSERT_EQ(3, errors.size());
 
-    EXPECT_EQ(std::string{"http://example.org/geometry#radius"}, errors->at(0).property.iri());
+    EXPECT_EQ(std::string{"http://example.org/geometry#radius"}, errors.at(0).property.iri());
     EXPECT_EQ(std::string{"'myShape' property 'radius' has 0 distinct values. Minimum allowed is 1"},
-              errors->at(0).fullMessage());
-    EXPECT_EQ(Validator::Error::NOTENOUHVALUES, errors->at(0).type);
+              errors.at(0).fullMessage());
+    EXPECT_EQ(validation::Error::NOTENOUHVALUES, errors.at(0).type);
 
-    EXPECT_EQ(std::string{"http://example.org/geometry#center"}, errors->at(1).property.iri());
+    EXPECT_EQ(std::string{"http://example.org/geometry#center"}, errors.at(1).property.iri());
     EXPECT_EQ(std::string{"'myShape' property 'center' has 0 distinct values. Minimum allowed is 1"},
-              errors->at(1).fullMessage());
-    EXPECT_EQ(Validator::Error::NOTENOUHVALUES, errors->at(1).type);
+              errors.at(1).fullMessage());
+    EXPECT_EQ(validation::Error::NOTENOUHVALUES, errors.at(1).type);
 
-    EXPECT_EQ(std::string{"http://example.org/geometry#borderThickness"}, errors->at(2).property.iri());
+    EXPECT_EQ(std::string{"http://example.org/geometry#borderThickness"}, errors.at(2).property.iri());
     EXPECT_EQ(std::string{"'myShape' property 'borderThickness' value 'titi' is not convertible as xsd:integer"},
-              errors->at(2).fullMessage());
-    EXPECT_EQ(Validator::Error::INVALIDDATATYPE, errors->at(2).type);
+              errors.at(2).fullMessage());
+    EXPECT_EQ(validation::Error::INVALIDDATATYPE, errors.at(2).type);
 }
 
 TEST_F(ValidatorTest, DataType) {
     const Object obj("http://example.org/geometry#point");
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj);
+    const auto errors = validation::validateObject(*ontology, obj);
     dumpErrors(errors);
 
-    ASSERT_EQ(1, errors->size());
+    ASSERT_EQ(1, errors.size());
 
-    EXPECT_EQ("http://example.org/geometry#x", errors->at(0).property.iri());
-    EXPECT_EQ("xsd:double", errors->at(0).range);
+    EXPECT_EQ("http://example.org/geometry#x", errors.at(0).property.iri());
+    EXPECT_EQ("xsd:double", errors.at(0).range);
     EXPECT_EQ(std::string{"'point' property 'x' value 'toto' is not convertible as xsd:double"},
-              errors->at(0).fullMessage());
-    EXPECT_EQ(Validator::Error::INVALIDDATATYPE, errors->front().type);
+              errors.at(0).fullMessage());
+    EXPECT_EQ(validation::Error::INVALIDDATATYPE, errors.front().type);
 }
 
 TEST_F(ValidatorTest, MinCardinality) {
     const Object obj("http://example.org/geometry#circle");
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj);
+    const auto errors = validation::validateObject(*ontology, obj);
     dumpErrors(errors);
 
-    ASSERT_EQ(2, errors->size());
+    ASSERT_EQ(2, errors.size());
 
-    EXPECT_EQ("http://example.org/geometry#radius", std::string{errors->at(0).property.iri()});
-    EXPECT_EQ(std::string{}, std::string{errors->at(0).range});
-    EXPECT_EQ("'circle' property 'radius' has 0 distinct values. Minimum allowed is 1", errors->at(0).fullMessage());
-    EXPECT_EQ(Validator::Error::NOTENOUHVALUES, errors->front().type);
+    EXPECT_EQ("http://example.org/geometry#radius", std::string{errors.at(0).property.iri()});
+    EXPECT_EQ(std::string{}, std::string{errors.at(0).range});
+    EXPECT_EQ("'circle' property 'radius' has 0 distinct values. Minimum allowed is 1", errors.at(0).fullMessage());
+    EXPECT_EQ(validation::Error::NOTENOUHVALUES, errors.front().type);
 
-    EXPECT_EQ("http://example.org/geometry#borderThickness", std::string{errors->at(1).property.iri()});
-    EXPECT_EQ(std::string{}, std::string{errors->at(1).range});
+    EXPECT_EQ("http://example.org/geometry#borderThickness", std::string{errors.at(1).property.iri()});
+    EXPECT_EQ(std::string{}, std::string{errors.at(1).range});
     EXPECT_EQ("'circle' property 'borderThickness' has 0 distinct values. Minimum allowed is 1",
-              errors->at(1).fullMessage());
-    EXPECT_EQ(Validator::Error::NOTENOUHVALUES, errors->at(1).type);
+              errors.at(1).fullMessage());
+    EXPECT_EQ(validation::Error::NOTENOUHVALUES, errors.at(1).type);
 }
 
 TEST_F(ValidatorTest, MaxCardinality) {
     const Object obj("http://example.org/geometry#rectangle");
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj);
+    const auto errors = validation::validateObject(*ontology, obj);
     dumpErrors(errors);
 
-    ASSERT_EQ(2, errors->size());
+    ASSERT_EQ(2, errors.size());
 
-    EXPECT_EQ("http://example.org/geometry#topLeft", std::string{errors->at(0).property.iri()});
-    EXPECT_EQ(std::string{}, std::string{errors->at(0).range});
+    EXPECT_EQ("http://example.org/geometry#topLeft", std::string{errors.at(0).property.iri()});
+    EXPECT_EQ(std::string{}, std::string{errors.at(0).range});
     EXPECT_EQ("'rectangle' property 'topLeft' has 2 distinct values. Maximum allowed is 1",
-              errors->at(0).fullMessage());
-    EXPECT_EQ(Validator::Error::TOOMANYVALUES, errors->at(0).type);
+              errors.at(0).fullMessage());
+    EXPECT_EQ(validation::Error::TOOMANYVALUES, errors.at(0).type);
 
-    EXPECT_EQ("http://example.org/geometry#borderThickness", std::string{errors->at(1).property.iri()});
-    EXPECT_EQ(std::string{}, std::string{errors->at(1).range});
+    EXPECT_EQ("http://example.org/geometry#borderThickness", std::string{errors.at(1).property.iri()});
+    EXPECT_EQ(std::string{}, std::string{errors.at(1).range});
     EXPECT_EQ("'rectangle' property 'borderThickness' has 0 distinct values. Minimum allowed is 1",
-              errors->at(1).fullMessage());
-    EXPECT_EQ(Validator::Error::NOTENOUHVALUES, errors->at(1).type);
+              errors.at(1).fullMessage());
+    EXPECT_EQ(validation::Error::NOTENOUHVALUES, errors.at(1).type);
 }
 
 TEST_F(ValidatorTest, Type) {
     const Object obj("http://example.org/geometry#objectType");
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj);
+    const auto errors = validation::validateObject(*ontology, obj);
     dumpErrors(errors);
 
-    ASSERT_EQ(3, errors->size());
+    ASSERT_EQ(3, errors.size());
 
-    EXPECT_EQ("http://example.org/geometry#center", std::string{errors->at(0).property.iri()});
-    EXPECT_EQ("http://example.org/geometry#Point", std::string{errors->at(0).range});
+    EXPECT_EQ("http://example.org/geometry#center", std::string{errors.at(0).property.iri()});
+    EXPECT_EQ("http://example.org/geometry#Point", std::string{errors.at(0).range});
     EXPECT_EQ(
             "'http://example.org/point/center' property 'center' has no type, while RDF expected type is http://example.org/geometry#Point",
-            errors->at(0).fullMessage());
-    EXPECT_EQ(Validator::Error::INVALIDTYPE, errors->at(0).type);
+            errors.at(0).fullMessage());
+    EXPECT_EQ(validation::Error::INVALIDTYPE, errors.at(0).type);
 
-    EXPECT_EQ("http://example.org/geometry#borderThickness", std::string{errors->at(1).property.iri()});
-    EXPECT_EQ(std::string{}, std::string{errors->at(1).range});
+    EXPECT_EQ("http://example.org/geometry#borderThickness", std::string{errors.at(1).property.iri()});
+    EXPECT_EQ(std::string{}, std::string{errors.at(1).range});
     EXPECT_EQ(
             "'objectType' property 'borderThickness' has 0 distinct values. Minimum allowed is 1",
-            errors->at(1).fullMessage());
-    EXPECT_EQ(Validator::Error::NOTENOUHVALUES, errors->at(1).type);
+            errors.at(1).fullMessage());
+    EXPECT_EQ(validation::Error::NOTENOUHVALUES, errors.at(1).type);
 
-    EXPECT_EQ("http://example.org/geometry#center", std::string{errors->at(2).property.iri()});
-    EXPECT_EQ("http://example.org/geometry#Point", std::string{errors->at(2).range});
+    EXPECT_EQ("http://example.org/geometry#center", std::string{errors.at(2).property.iri()});
+    EXPECT_EQ("http://example.org/geometry#Point", std::string{errors.at(2).range});
     EXPECT_EQ(
             "'http://example.org/point/center' property 'center' has no type, while RDF expected type is http://example.org/geometry#Point",
-            errors->at(2).fullMessage());
-    EXPECT_EQ(Validator::Error::INVALIDTYPE, errors->at(2).type);
+            errors.at(2).fullMessage());
+    EXPECT_EQ(validation::Error::INVALIDTYPE, errors.at(2).type);
 
 }
 
 TEST_F(ValidatorTest, UniqueKeyValidator) {
     const Object obj("http://example.org/geometry#rectangle1");
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj);
-    ASSERT_EQ(0, errors->size());
+    const auto errors = validation::validateObject(*ontology, obj);
+    ASSERT_EQ(0, errors.size());
 }
 
 TEST_F(ValidatorTest, UniqueKeyValidatorFail) {
     const Object obj("http://example.org/geometry#schema1");
 
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj);
-    EXPECT_EQ(0, errors->size());
+    auto errors = validation::validateObject(*ontology, obj);
+    EXPECT_EQ(0, errors.size());
 
-    Validator::ValidationOption option(true, false);
-    errors = validator->validateObject(obj, option);
-    ASSERT_EQ(1, errors->size());
-    EXPECT_EQ(Validator::Error::DUPLICATEDVALUESKEY, errors->at(0).type);
-    EXPECT_EQ("\'Schema\' class key \'schemaUniqueName\' has the duplicated value \'sharedSchemaKey\'", errors->at(0).fullMessage());
+    validation::ValidationOption option(true, false);
+    errors = validation::validateObject(*ontology, obj, option);
+    ASSERT_EQ(1, errors.size());
+    EXPECT_EQ(validation::Error::DUPLICATEDVALUESKEY, errors.at(0).type);
+    EXPECT_EQ("\'Schema\' class key \'schemaUniqueName\' has the duplicated value \'sharedSchemaKey\'", errors.at(0).fullMessage());
 
     // The error is indeed catched at runtime
     EXPECT_THROW(
@@ -185,32 +183,32 @@ TEST_F(ValidatorTest, UniqueKeyValidatorFail) {
 TEST_F(ValidatorTest, UniqueKeyValidatorFailForOtherInstance) {
     const Object obj("http://example.org/geometry#schema2");
 
-    Validator::ValidationOption option(true, false);
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj, option);
-    ASSERT_EQ(1, errors->size());
-    EXPECT_EQ(Validator::Error::DUPLICATEDVALUESKEY, errors->at(0).type);
-    EXPECT_EQ("\'Schema\' class key \'schemaUniqueName\' has the duplicated value \'sharedSchemaKey\'", errors->at(0).fullMessage());
+    validation::ValidationOption option(true, false);
+    const auto errors = validation::validateObject(*ontology, obj, option);
+    ASSERT_EQ(1, errors.size());
+    EXPECT_EQ(validation::Error::DUPLICATEDVALUESKEY, errors.at(0).type);
+    EXPECT_EQ("\'Schema\' class key \'schemaUniqueName\' has the duplicated value \'sharedSchemaKey\'", errors.at(0).fullMessage());
 }
 
 TEST_F(ValidatorTest, DomainErrors) {
     const Object obj("http://example.org/geometry#schema2");
 
-    Validator::ValidationOption option(false, true);
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateObject(obj, option);
-    ASSERT_EQ(2, errors->size());
-    EXPECT_EQ(Validator::Error::INVALIDDOMAIN, errors->at(0).type);
-    EXPECT_EQ("Thing class should be in the 'AutoRDF:ordered' domain", errors->at(0).fullMessage());
+    validation::ValidationOption option(false, true);
+    const auto errors = validation::validateObject(*ontology, obj, option);
+    ASSERT_EQ(2, errors.size());
+    EXPECT_EQ(validation::Error::INVALIDDOMAIN, errors.at(0).type);
+    EXPECT_EQ("Thing class should be in the 'AutoRDF:ordered' domain", errors.at(0).fullMessage());
 }
 
 TEST_F(ValidatorTest, ModelValidator) {
-    Validator::ValidationOption option(true, true);
-    std::shared_ptr<std::vector<Validator::Error>> errors = validator->validateModel(factory, option);
+    validation::ValidationOption option(true, true);
+    auto errors = validation::validateModel(*ontology, option);
     dumpErrors(errors);
-    EXPECT_EQ(64, errors->size());
+    EXPECT_EQ(64, errors.size());
 
-    errors = validator->validateModel(factory);
+    errors = validation::validateModel(*ontology);
     dumpErrors(errors);
-    EXPECT_EQ(16, errors->size());
+    EXPECT_EQ(16, errors.size());
 }
 
 
